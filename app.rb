9c6b6095959d72896991780ca06a102f593a342b
Greatmedivack <putinihin@medods.ru>
2020-02-06 17:52:42 +0500

    releaseversions

--- app.rb
+++ app.rb
@@ -5,32 +5,39 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 @@connection = PG.connect :dbname => 'container_version_base',
-                         :host => 'db',
+                         :host => 'pgbouncer',
                          :user => 'postgres',
                          :password => '10241024'
 
- 
-
 get '/' do
-  services = @@connection.exec 'SELECT * FROM container_versions'
+  redirect to('/list/v39')
+end
+
+get '/list/:release' do
+  params['release'] ||= "v39"
+  services = @@connection.exec "SELECT * FROM container_versions WHERE release_version = \'#{params['release']}\'"
   erb :containers, :locals => {:services => services}
 end
 
+
+
 post '/update' do
-  service = @@connection.exec "SELECT * FROM container_versions \
-                              WHERE application_name = \'#{params['service']}\' \
-                              LIMIT 1" 
   
   if  params['service'].nil? || params['version'].nil? || params['description'].nil? || params['release'].nil?
     status 403
     body "Error" 
     return
   end
+  
+  service = @@connection.exec "SELECT * FROM container_versions \
+                              WHERE application_name = \'#{params['service']}\' AND release_version = \'#{params['release']}\' \
+                              LIMIT 1" 
+  
 
   unless service.values.empty?     
     @@connection.exec "UPDATE container_versions \
                      SET application_version = \'#{params['version']}\' \
-                     WHERE application_name = \'#{params['service']}\'"
+                     WHERE application_name = \'#{params['service']}\' AND release_version = \'#{params['release']}\'"
     status 200
     body "Complete"
   else
