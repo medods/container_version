a9e47a09b6a857039c2975d2727af56b241c0da3
Greatmedivack <putinihin@medods.ru>
2020-02-07 19:06:19 +0500

    css

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 @@connection = PG.connect :dbname => 'container_version_base',
-                         :host => 'pgbouncer',
-                         :user => 'postgres',
-                         :password => '10241024'
+                          :host => 'pgbouncer',
+                          :user => 'postgres',
+                          :password => '10241024'
 
 get '/' do
   redirect to('/list/v39')
@@ -15,7 +15,11 @@ end
 
 get '/list/:release' do
   params['release'] ||= "v39"
-  services = @@connection.exec "SELECT * FROM container_versions WHERE release_version = \'#{params['release']}\'"
+  services = @@connection.exec  %Q( SELECT * 
+                                    FROM container_versions 
+                                    WHERE 
+                                      release_version = \'#{params['release']}\')  
+
   erb :containers, :locals => {:services => services}
 end
 
@@ -29,25 +33,35 @@ post '/update' do
     return
   end
   
-  service = @@connection.exec "SELECT * FROM container_versions \
-                              WHERE application_name = \'#{params['service']}\' AND release_version = \'#{params['release']}\' \
-                              LIMIT 1" 
+  service = @@connection.exec %Q( SELECT * 
+                                  FROM container_versions
+                                  WHERE 
+                                    application_name = \'#{params['service']}\' 
+                                  AND  
+                                    release_version = \'#{params['release']}\'
+                                  LIMIT 1) 
   
 
   unless service.values.empty?     
-    @@connection.exec "UPDATE container_versions \
-                     SET application_version = \'#{params['version']}\' \
-                     WHERE application_name = \'#{params['service']}\' AND release_version = \'#{params['release']}\'"
+    @@connection.exec %Q( UPDATE container_versions 
+                          SET
+                            application_version = \'#{params['version']}\'
+                          WHERE 
+                            application_name = \'#{params['service']}\' 
+                          AND 
+                            release_version = \'#{params['release']}\')
     status 200
     body "Complete"
   else
-    @@connection.exec "INSERT INTO container_versions \
-                     (release_version, application_name, \
-                     application_version, description) \
-                     VALUES (\'#{params['release']}\', \
-                     \'#{params['service']}\', \
-                     \'#{params['version']}\', \
-                     \'#{params['description']}\')"
+    @@connection.exec %Q( INSERT INTO 
+                            container_versions
+                            (release_version, application_name, 
+                             application_version, description) 
+                          VALUES 
+                            (\'#{params['release']}\', 
+                             \'#{params['service']}\', 
+                             \'#{params['version']}\', 
+                             \'#{params['description']}\'))
     status 200
     body "Complete"
  end
